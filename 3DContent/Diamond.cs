7150680bf8985a4996e3096331adee599dc8c4b1
Easton Bolinger <bolinger@ksu.edu>
2022-04-15 19:46:35 -0500

    base diamond implemented

--- 3DContent/Diamond.cs
+++ 3DContent/Diamond.cs
@@ -24,19 +24,54 @@ namespace ChickenFarm
 
         void InitializeVerticies()
         {
-            vertices = new VertexPositionColor[3];
+            vertices = new VertexPositionColor[12];
 
-            //vertex 0
-            vertices[0].Position = new Vector3(0, 1, 0);
-            vertices[0].Color = Color.Red;
+            //vertex 0 left
+            vertices[0].Position = new Vector3(-1, 0, 0);
+            vertices[0].Color = Color.Blue;
 
-            //vertex 1
-            vertices[1].Position = new Vector3(1, 1, 0);
-            vertices[1].Color = Color.Red;
+            //vertex 1 center
+            vertices[1].Position = new Vector3(0, 0, 0);
+            vertices[1].Color = Color.Green;
 
-            //vertex 2
-            vertices[2].Position = new Vector3(1, 0, 0);
+            //vertex 2 bottom
+            vertices[2].Position = new Vector3(0, -1, 0);
             vertices[2].Color = Color.Red;
+
+            //vertex 3 left
+            vertices[3].Position = vertices[0].Position;
+            vertices[3].Color = Color.Blue;
+
+            //vertex 4 center
+            vertices[4].Position = vertices[1].Position;
+            vertices[4].Color = vertices[1].Color;
+
+            //vertex 5 top
+            vertices[5].Position = new Vector3(0, 1, 0);
+            vertices[5].Color = Color.Red;
+
+            //vertex 6 center
+            vertices[6].Position = vertices[1].Position;
+            vertices[6].Color = Color.Green;
+
+            //vertex 7 bottom
+            vertices[7].Position = vertices[2].Position;
+            vertices[7].Color = Color.Red;
+
+            //vertex 8 right
+            vertices[8].Position = new Vector3(1, 0, 0);
+            vertices[8].Color = Color.Blue;
+
+            //vertex 9 center
+            vertices[9].Position = vertices[1].Position;
+            vertices[9].Color = Color.Green;
+
+            //vertex 10 top
+            vertices[10] = vertices[5];
+
+            //vertex 11 right
+            vertices[11] = vertices[8];
+
         }
 
         void InitializeEffect()
@@ -52,7 +87,7 @@ namespace ChickenFarm
                 MathHelper.PiOver4,
                 game.GraphicsDevice.Viewport.AspectRatio,
                 0.1f,
-                100.0f
+                50.0f
             );
             effect.VertexColorEnabled = true;
         }
@@ -79,7 +114,7 @@ namespace ChickenFarm
                 PrimitiveType.TriangleList,
                 vertices,
                 0,
-                1
+                4
             );
 
             game.GraphicsDevice.RasterizerState = oldState;
