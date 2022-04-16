29c8c5f396248a02477b7aa9a5278dbfde19057a
Easton Bolinger <bolinger@ksu.edu>
2022-04-15 20:02:46 -0500

    z side of diamond started

--- 3DContent/Diamond.cs
+++ 3DContent/Diamond.cs
@@ -24,54 +24,64 @@ namespace ChickenFarm
 
         void InitializeVerticies()
         {
-            vertices = new VertexPositionColor[12];
-
+            vertices = new VertexPositionColor[15];
+            #region verticies 0-11: x - y face of diamond
             //vertex 0 left
             vertices[0].Position = new Vector3(-1, 0, 0);
-            vertices[0].Color = Color.Blue;
+            vertices[0].Color = Color.LightGreen;
 
             //vertex 1 center
             vertices[1].Position = new Vector3(0, 0, 0);
-            vertices[1].Color = Color.Green;
+            vertices[1].Color = Color.DarkGreen;
 
             //vertex 2 bottom
             vertices[2].Position = new Vector3(0, -1, 0);
-            vertices[2].Color = Color.Red;
+            vertices[2].Color = Color.LightGreen;
 
             //vertex 3 left
-            vertices[3].Position = vertices[0].Position;
-            vertices[3].Color = Color.Blue;
+            vertices[3] = vertices[0];
 
             //vertex 4 center
-            vertices[4].Position = vertices[1].Position;
-            vertices[4].Color = vertices[1].Color;
+            vertices[4] = vertices[1];
 
             //vertex 5 top
             vertices[5].Position = new Vector3(0, 1, 0);
-            vertices[5].Color = Color.Red;
+            vertices[5].Color = Color.LightGreen;
 
             //vertex 6 center
-            vertices[6].Position = vertices[1].Position;
-            vertices[6].Color = Color.Green;
+            vertices[6] = vertices[1];
 
             //vertex 7 bottom
-            vertices[7].Position = vertices[2].Position;
-            vertices[7].Color = Color.Red;
+            vertices[7] = vertices[2];
 
             //vertex 8 right
             vertices[8].Position = new Vector3(1, 0, 0);
-            vertices[8].Color = Color.Blue;
+            vertices[8].Color = Color.LightGreen;
 
             //vertex 9 center
-            vertices[9].Position = vertices[1].Position;
-            vertices[9].Color = Color.Green;
+            vertices[9] = vertices[1];
 
             //vertex 10 top
             vertices[10] = vertices[5];
 
             //vertex 11 right
             vertices[11] = vertices[8];
+            #endregion
 
+            #region verticies 12 - 24 z face of diamond
+            //vertex 12 back center
+            vertices[12].Position = new Vector3(0, 0, -1);
+            vertices[12].Color = Color.LightGreen;
+
+            //vertex 13 center
+            vertices[13].Position = new Vector3(0, 0, 0);
+            vertices[13].Color = Color.DarkGreen;
+
+            //vertex 14 back bottom
+            vertices[14].Position = new Vector3(0, -1, 0);
+            vertices[14].Color = Color.LightGreen;
+
+            #endregion
         }
 
         void InitializeEffect()
@@ -114,7 +124,7 @@ namespace ChickenFarm
                 PrimitiveType.TriangleList,
                 vertices,
                 0,
-                4
+                5
             );
 
             game.GraphicsDevice.RasterizerState = oldState;
